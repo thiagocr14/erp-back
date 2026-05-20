f34f6d8a38700f1e91e8e7223f0f54116850db08
thiago <thiagocruz5123@gmail.com>
2026-05-20 16:05:11 -0300

    fix: salvar orcamentocontroller

--- ErpAcademico.WebApi/Controllers/OrcamentoController.cs
+++ ErpAcademico.WebApi/Controllers/OrcamentoController.cs
@@ -2,51 +2,33 @@ using ErpAcademico.Application.DTOs;
 using ErpAcademico.Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-
 namespace ErpAcademico.WebApi.Controllers;
-
 [ApiController]
 [Route("api/[controller]")]
 [Authorize]
 public class OrcamentosController : ControllerBase
 {
     private readonly OrcamentoService _orcamentoService;
-
-    public OrcamentosController(
-        OrcamentoService orcamentoService)
+    public OrcamentosController(OrcamentoService orcamentoService)
     {
         _orcamentoService = orcamentoService;
     }
-
-    [HttpPost]
-    public async Task<IActionResult> CriarOrcamento(
-        [FromBody] CriarOrcamentoDto dto)
+    [HttpGet]
+    public async Task<IActionResult> ObterTodos()
     {
-        var orcamentoId =
-            await _orcamentoService
-                .CriarOrcamento(dto);
-
-        return Ok(new
-        {
-            mensagem = "Orçamento criado com sucesso.",
-            orcamentoId
-        });
+        var orcamentos = await _orcamentoService.ObterTodos();
+        return Ok(orcamentos);
     }
-
-
-[HttpPost("{id}/converter")]
-public async Task<IActionResult> ConverterEmVenda(
-    Guid id)
-{
-    var vendaId =
-        await _orcamentoService
-            .ConverterEmVenda(id);
-
-    return Ok(new
+    [HttpPost]
+    public async Task<IActionResult> CriarOrcamento([FromBody] CriarOrcamentoDto dto)
     {
-        mensagem =
-            "Orçamento convertido em venda com sucesso.",
-        vendaId
-    });
-}
+        var orcamentoId = await _orcamentoService.CriarOrcamento(dto);
+        return Ok(new { mensagem = "Orçamento criado com sucesso.", orcamentoId });
+    }
+    [HttpPost("{id}/converter")]
+    public async Task<IActionResult> ConverterEmVenda(Guid id)
+    {
+        var vendaId = await _orcamentoService.ConverterEmVenda(id);
+        return Ok(new { mensagem = "Orçamento convertido em venda com sucesso.", vendaId });
+    }
 }
